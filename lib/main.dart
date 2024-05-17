8e104f703a3be0aea673c2a7e4449a0a7b19f9d9
Your Name <you@example.com>
2024-05-17 06:35:22 +0300

    Initial project

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:e_commerce_app/ui/splash/splashscreen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(430,932),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          initialRoute: Splash.routeName,
+          routes:{
+           Splash.routeName:(context) => Splash(),
+          },
+        );
+      }
+    );
+  }
+}
+
---
+++ lib/ui/splash/splashscreen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Splash extends StatelessWidget {
+  static const String routeName = 'SplashScreen';
+  const Splash({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
